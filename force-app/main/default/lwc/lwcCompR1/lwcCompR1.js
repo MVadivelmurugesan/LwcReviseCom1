a7dd336916a3bb93174b75c2e414d3abb16d5909
MVadivelmurugesan <mvadivel5427@gmail.com>
2024-10-21 11:22:40 +0530

    Lwcpro1

---
+++ force-app/main/default/lwc/lwcCompR1/lwcCompR1.js
@@ -0,0 +1,10 @@
+import { LightningElement, track } from 'lwc';
+import accrecords from '@salesforce/apex/lwcaccountcls1.getAccount';
+export default class LwcCompR1 extends LightningElement {
+
+    @track Accountdata=[];
+    error='';
+    async function () {
+        
+    }
+}
