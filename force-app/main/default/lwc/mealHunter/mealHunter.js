fba38e973db6e92e7edeb58fd5b634c10806084c
MVadivelmurugesan <mvadivel5427@gmail.com>
2024-10-28 16:23:51 +0530

    Final change

--- force-app/main/default/lwc/mealHunter/mealHunter.js
+++ force-app/main/default/lwc/mealHunter/mealHunter.js
@@ -1,28 +1,27 @@
-import { LightningElement } from 'lwc';
-import fetchmeal from '@salesforce/apex/MealSerivces.fetchmeal'
+import { LightningElement, track } from 'lwc';
+import fetchmeal from '@salesforce/apex/MealServices.fetchmeal';
 
 export default class MealHunter extends LightningElement {
+    @track searchresult = [];
 
-    searchresult;
     async mealsearchhandler(event) {
         let searchmeal = event.detail;
-        console.log('SearchMeal : ', searchmeal);
+        console.log('SearchMeal:', searchmeal);
 
         try {
-           let  result=await fetchmeal({meal : searchmeal });
-           let  data=JSON.parse(result);
-           console.log('data : ',data);
-           console.log('data :==> ',data.meals);
-           if (data.length > 0) {
-                this.searchresult=data.meals;
-           }else{
-            console.log('no meals');
-           }
+            let result = await fetchmeal({ meal: searchmeal });
+            let data = JSON.parse(result);
+            console.log('data:', data);
 
-            
+            if (data && data.meals) {
+                this.searchresult = data.meals;
+                console.log('Search Result:', this.searchresult);
+            } else {
+                console.log('No meals found.');
+                this.searchresult = [];
+            }
         } catch (error) {
             console.error('Error fetching data:', error);
         }
     }
-
 }
--- force-app/main/default/lwc/mealLoad/mealLoad.js
+++ force-app/main/default/lwc/mealLoad/mealLoad.js
@@ -1,9 +1,16 @@
-import { LightningElement ,api} from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class MealLoad extends LightningElement {
-    @api mealresult =[];
+    @api mealresult = [];
 
-    get checkmeal(){
+    get checkmeal() {
         return this.mealresult.length > 0;
+    }
+
+    recipehandler(event) {
+        const mealId = event.target.dataset.id;
+        console.log('Meal ID:', mealId);
+        const selectedMeal = this.mealresult.find(meal => meal.idMeal === mealId);
+        console.log('Selected Meal:', selectedMeal);
     }
 }
