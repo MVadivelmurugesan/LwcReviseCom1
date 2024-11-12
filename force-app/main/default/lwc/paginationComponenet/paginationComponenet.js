2c0f2c2f34629f2d291e9dda2a3b85fa66e4ce75
MVadivelmurugesan <mvadivel5427@gmail.com>
2024-11-12 11:52:12 +0530

    Pagination Code

---
+++ force-app/main/default/lwc/paginationComponenet/paginationComponenet.js
@@ -0,0 +1,53 @@
+import { LightningElement,api,track } from 'lwc';
+
+export default class PaginationComponenet extends LightningElement {
+    @api currentPage = 1;
+    @api totalPages = 1;
+
+    get pages() {
+        let pages = [];
+        for (let i = 1; i <= this.totalPages; i++) {
+            pages.push({
+                label: i,
+                value: i,
+                variant: this.currentPage === i ? 'brand' : 'neutral'
+            });
+        }
+        return pages;
+    }
+
+    get isPreviousDisabled() {
+        return this.currentPage === 1;
+    }
+
+    get isNextDisabled() {
+        return this.currentPage === this.totalPages;
+    }
+
+    previousPage() {
+        if (this.currentPage > 1) {
+            this.currentPage--;
+            this.notifyPageChange();
+        }
+    }
+
+    nextPage() {
+        if (this.currentPage < this.totalPages) {
+            this.currentPage++;
+            this.notifyPageChange();
+        }
+    }
+
+    handlePageClick(event) {
+        const selectedPage = parseInt(event.target.value, 10);
+        if (selectedPage !== this.currentPage) {
+            this.currentPage = selectedPage;
+            this.notifyPageChange();
+        }
+    }
+
+    notifyPageChange() {
+        const pageChangeEvent = new CustomEvent('pagechange', { detail: this.currentPage });
+        this.dispatchEvent(pageChangeEvent);
+    }
+}
